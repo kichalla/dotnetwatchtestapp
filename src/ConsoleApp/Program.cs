65ba2688be0c913d23bd2b333552fe93c397c074
Kiran Challa <kichalla@microsoft.com>
2016-05-04 12:13:11 -0700

    Added console logging

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace ConsoleApp
 {
@@ -11,6 +12,11 @@ namespace ConsoleApp
         {
             var c = new ClassLibrary1.Class1();
             Console.WriteLine(c.GetGreeting());
+
+            var loggerFactory = new LoggerFactory()
+                .AddConsole();
+            var logger = loggerFactory.CreateLogger<Program>();
+            logger.LogInformation("A message from Program");
         }
     }
 }
